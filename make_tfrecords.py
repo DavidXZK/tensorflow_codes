8b16df888b4cf6ea8e5aef1d0a9945aa6f1ce907
DavidX <thumaskman@gmail.com>
2019-05-05 18:59:03 +0800

    modify make_tfrecords.py

--- make_tfrecords.py
+++ make_tfrecords.py
@@ -9,7 +9,7 @@ def get_filenames(file_dir):
     '''return filenames and labels'''
     for root, dirs, files in os.walk(file_dir):
         print(root, dirs)
-        labels = [int(fn.replace('.jpg', '').split('_')[1]) for fn in files]
+        labels = [int(fn.replace('.jpg', '').split('_')[-1]) for fn in files]
         assert(len(files) != 0)
         return files, labels
 
@@ -18,16 +18,16 @@ def write_to_tfrecords(file_dir, record_dir, record_name):
     filenames, labels = get_filenames(file_dir)
     count = 0
     for imagename, label in zip(filenames, labels):
-         img_path = file_dir + '/' + imagename
-         img = Image.open(img_path)
-         img_raw = img.tobytes()
-         feature_label = tf.train.Feature(int64_list=tf.train.Int64List(value=[label]))
-         feature_img = tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
-         example = tf.train.Example(features=tf.train.Features(feature={'label':feature_label, 'img_raw':feature_img}))
-         writer.write(example.SerializeToString())
-         count += 1
-         if count % 1000 == 0:
-             print('write %d images'%(count))
+        img_path = file_dir + '/' + imagename
+        img = Image.open(img_path)
+        img_raw = img.tobytes()
+        feature_label = tf.train.Feature(int64_list=tf.train.Int64List(value=[label]))
+        feature_img = tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+        example = tf.train.Example(features=tf.train.Features(feature={'label':feature_label, 'img_raw':feature_img}))
+        writer.write(example.SerializeToString())
+        count += 1
+        if count % 1000 == 0:
+            print('write %d images'%(count))
     writer.close()
 
 if __name__ == '__main__':
